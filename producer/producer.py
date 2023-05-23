06a1233efc86f295f5d0af0b5f91130ec9f8be49
xavierdebeaumont <xdebeaumont@outlook.fr>
2023-05-23 11:31:21 +0200

    first commit

---
+++ producer/producer.py
@@ -0,0 +1,59 @@
+import argparse
+import json
+from confluent_kafka import Producer
+import tweepy
+import os 
+
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--bearer_token', type=str, required=True)
+    parser.add_argument('--stream_rule', nargs='+', type=str, required=True)
+    parser.add_argument('--topic_id', type=str, required=True)
+
+    return parser.parse_args()
+
+# Kafka configuration
+# KAFKA_ADDRESS = os.getenv('KAFKA_ADDRESS')
+kafka_bootstrap_servers = 'broker:9092'
+
+def write_to_kafka(data, producer, stream_rule, kafka_topic):
+    data["stream_rule"] = stream_rule
+    data_formatted = json.dumps(data).encode("UTF-8")
+    producer.produce(kafka_topic, data_formatted)
+    producer.flush()
+
+# Custom StreamListener class to process tweets and send to Kafka
+class KafkaStreamListener(tweepy.StreamingClient):
+    def __init__(self, kafka_topic, bearer_token, stream_rule):
+        super().__init__(bearer_token)
+        self.kafka_topic = kafka_topic
+        self.producer = Producer({'bootstrap.servers': kafka_bootstrap_servers})
+        self.stream_rule = stream_rule
+
+    def on_response(self, response):
+        tweet_data = response.data.data
+
+        user_data = response.includes['users'][0].data
+        result = tweet_data
+        result["user"] = user_data
+
+        print(result)
+        write_to_kafka(result, self.producer, self.stream_rule, self.kafka_topic)
+
+    def on_error(self, status):
+        print('Twitter API error:', status)
+
+if __name__ =="__main__":
+    tweet_fields = ['id', 'text', 'author_id', 'created_at', 'lang']
+    user_fields = ['description', 'created_at', 'location']
+    expansions = ['author_id']
+
+    args = parse_args()
+    stream_listener = KafkaStreamListener(args.topic_id, args.bearer_token, args.stream_rule)
+    print(args.topic_id + '\n')
+
+    stream_listener.add_rules(tweepy.StreamRule(args.stream_rule))
+    stream_listener.filter(tweet_fields=tweet_fields, expansions=expansions, user_fields=user_fields)
---
+++ terraform/variables.tf
@@ -0,0 +1,56 @@
+locals {
+  data_lake_bucket = "dtc_data_lake"
+}
+
+variable "project" {
+  description = "Your GCP Project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "Your project region"
+  default     = "europe-west9"
+  type        = string
+}
+
+variable "zone" {
+  description = "Your project zone"
+  default     = "europe-west9-a"
+  type        = string
+}
+
+variable "storage_class" {
+  description = "Storage class type for your bucket"
+  default     = "STANDARD"
+  type        = string
+}
+
+variable "vm_image" {
+  description = "Image for you VM"
+  default     = "ubuntu-os-cloud/ubuntu-2004-lts"
+  type        = string
+}
+
+variable "network" {
+  description = "Network for your instance/cluster"
+  default     = "default"
+  type        = string
+}
+
+variable "credentials" {
+  description = "Your google credentials"
+  type        = string
+}
+
+variable "stg_bq_dataset" {
+  description = "Storage class type for your bucket. Check official docs for more info."
+  default     = "streaming_stg"
+  type        = string
+}
+
+variable "prod_bq_dataset" {
+  description = "Storage class type for your bucket. Check official docs for more info."
+  default     = "streaming_prod"
+  type        = string
+}
+
